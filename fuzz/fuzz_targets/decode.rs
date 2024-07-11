29096a1986eb19367d38c3d1dec0ded151cf5b3f
Ville Laurikari <ville@laurikari.net>
2024-07-11 17:28:18 +0300

    Fix fuzzing to work

--- fuzz/fuzz_targets/decode.rs
+++ fuzz/fuzz_targets/decode.rs
@@ -1,8 +1,8 @@
 #![no_main]
-use cryptid_rs::Codec;
+use cryptid_rs::{Codec, Config};
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
-    let codec = Codec::new("test", b"random-key");
+    let codec = Codec::new("test", &Config::new(b"random-key"));
     let _ = codec.decode(&String::from_utf8_lossy(data));
 });
