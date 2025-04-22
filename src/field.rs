d0f8261c29e38b3cb4361ad9832e76389a27f4fb
Ville Laurikari <ville@laurikari.net>
2025-04-22 07:53:41 +0000

    Add SQLx support as optional feature, make Diesel support optional

--- src/field.rs
+++ src/field.rs
@@ -3,13 +3,22 @@ use std::collections::HashMap;
 use std::fmt;
 use std::sync::Arc;
 
-use diesel::deserialize::{self, FromSql, Queryable};
-use diesel::expression::AsExpression;
-use diesel::pg::{Pg, PgValue};
-use diesel::serialize::{self, Output, ToSql};
-use diesel::sql_types::BigInt;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use uuid::Uuid;
+
+#[cfg(feature = "diesel")]
+use diesel::deserialize::{self, FromSql, Queryable};
+#[cfg(feature = "diesel")]
+use diesel::expression::AsExpression;
+#[cfg(feature = "diesel")]
+use diesel::pg::{Pg, PgValue};
+#[cfg(feature = "diesel")]
+use diesel::serialize::{self, Output, ToSql};
+#[cfg(feature = "diesel")]
+use diesel::sql_types::BigInt;
+
+#[cfg(feature = "sqlx")]
+use sqlx::{postgres::PgTypeInfo, Postgres, Type};
 
 use crate::{Codec, Config};
 
@@ -41,7 +50,7 @@ pub trait TypeMarker: std::fmt::Debug {
 /// to an integer.  The string has an object type specific prefix defined in
 /// the type marker's `fn name()`.
 ///
-/// Traits are also provided for Diesel compatibility with Postgres BigInt fields.
+/// Traits are also provided for both Diesel and SQLx compatibility with Postgres BigInt fields.
 ///
 /// # Examples
 ///
@@ -68,8 +77,9 @@ pub trait TypeMarker: std::fmt::Debug {
 /// let obj_str = serde_json::to_string(&obj).unwrap();
 /// assert_eq!(obj_str, "{\"id\":\"example_VgwPy6rwatl\"}");
 /// ```
-#[derive(AsExpression, Debug, Clone, Copy)]
-#[diesel(sql_type = BigInt)]
+#[cfg_attr(feature = "diesel", derive(AsExpression))]
+#[cfg_attr(feature = "diesel", diesel(sql_type = BigInt))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Field<T: TypeMarker> {
     id: u64,
     _marker: std::marker::PhantomData<T>,
@@ -117,6 +127,7 @@ impl<T: TypeMarker> Field<T> {
     }
 }
 
+// For queries with single values (not tables)
 impl<T: TypeMarker> Serialize for Field<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -128,25 +139,34 @@ impl<T: TypeMarker> Serialize for Field<T> {
     }
 }
 
+// For queries with single values (not tables)
 impl<'de, T: TypeMarker> Deserialize<'de> for Field<T> {
     fn deserialize<D>(deserializer: D) -> Result<Field<T>, D::Error>
     where
         D: Deserializer<'de>,
     {
-        let encoded = String::deserialize(deserializer)?;
+        use serde::de::Error;
+
+        let text = String::deserialize(deserializer)?;
         let codec_name = T::name();
         let codec = get_or_create_codec(codec_name);
-        let id = codec.decode(&encoded).map_err(serde::de::Error::custom)?;
-        Ok(Field::from(id))
+        let id = codec.decode(&text).map_err(|e| Error::custom(e))?;
+        Ok(Field {
+            id: id,
+            _marker: std::marker::PhantomData,
+        })
     }
 }
 
+// -- Diesel compatibility for postgres --
+#[cfg(feature = "diesel")]
 impl<T: TypeMarker> ToSql<BigInt, Pg> for Field<T> {
     fn to_sql(&self, out: &mut Output<'_, '_, Pg>) -> serialize::Result {
         <i64 as ToSql<BigInt, Pg>>::to_sql(&(self.id as i64), &mut out.reborrow())
     }
 }
 
+#[cfg(feature = "diesel")]
 impl<T: TypeMarker> FromSql<BigInt, Pg> for Field<T> {
     fn from_sql(bytes: PgValue<'_>) -> deserialize::Result<Self> {
         let id = <i64 as FromSql<BigInt, Pg>>::from_sql(bytes)?;
@@ -154,6 +174,7 @@ impl<T: TypeMarker> FromSql<BigInt, Pg> for Field<T> {
     }
 }
 
+#[cfg(feature = "diesel")]
 impl<T> Queryable<BigInt, Pg> for Field<T>
 where
     T: TypeMarker,
@@ -163,5 +184,36 @@ where
     fn build(row: Self::Row) -> deserialize::Result<Self> {
         let id = i64::build(row)?;
         Ok(Field::from(id as u64))
+    }
+}
+
+// -- SQLx compatibility for postgres --
+
+#[cfg(feature = "sqlx")]
+// Type implementation for SQLx
+impl<T: TypeMarker> Type<Postgres> for Field<T> {
+    fn type_info() -> PgTypeInfo {
+        <i64 as Type<Postgres>>::type_info()
+    }
+}
+
+#[cfg(feature = "sqlx")]
+// Encode implementation for SQLx (for parameters)
+impl<'q, T: TypeMarker> sqlx::Encode<'q, Postgres> for Field<T> {
+    fn encode_by_ref(&self, buf: &mut sqlx::postgres::PgArgumentBuffer) -> sqlx::encode::IsNull {
+        let id = self.id as i64;
+        <i64 as sqlx::Encode<Postgres>>::encode_by_ref(&id, buf)
+    }
+}
+
+#[cfg(feature = "sqlx")]
+// Decode implementation for SQLx (for query results)
+impl<'r, T: TypeMarker> sqlx::Decode<'r, Postgres> for Field<T> {
+    fn decode(value: sqlx::postgres::PgValueRef<'r>) -> Result<Self, sqlx::error::BoxDynError> {
+        let id = <i64 as sqlx::Decode<Postgres>>::decode(value)?;
+        Ok(Field {
+            id: id as u64,
+            _marker: std::marker::PhantomData,
+        })
     }
 }
